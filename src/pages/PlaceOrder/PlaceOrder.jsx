d833e1dd48ff0a2550fcd620e7df29308899cd5b
shubham02-raj <90085239+shubham02-raj@users.noreply.github.com>
2025-01-20 16:34:37 +0530

    Update PlaceOrder.jsx

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import "./PlaceOrder.css";
+import "./Placeorder.css";
 import { StoreContext } from "../../context/StoreContext";
 
 const PlaceOrder = () => {
