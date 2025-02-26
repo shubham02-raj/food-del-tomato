e80c768a7c40b2f0df2589e096c5dd747d28a149
shubham02-raj <90085239+shubham02-raj@users.noreply.github.com>
2025-02-26 16:08:57 +0530

    Update main.jsx with HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import StoreContextProvider from "./context/StoreContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <StoreContextProvider>
-      <App />
-    </StoreContextProvider>
-  </BrowserRouter>
+  <HashRouter>
+    <Router>
+      <StoreContextProvider>
+        <App />
+      </StoreContextProvider>
+    </Router>
+  </HashRouter>
 );
